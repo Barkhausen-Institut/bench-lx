2f38551cabcbdba7e06bbc0270dc64c2533411e9
Nils Asmussen <nils@script-solution.de>
2015-01-13 13:17:24 +0100

    Added a read-benchmark with mmap without copy.

--- apps/readmmapcpy/readmmapcpy.c
+++ apps/readmmapcpy/readmmapcpy.c
@@ -6,34 +6,43 @@
 #include <unistd.h>
 #include <cycles.h>
 
+static void copy(void *dst, const void *src, size_t size) {
+    unsigned *d = (unsigned*)dst;
+    unsigned *s = (unsigned*)src;
+    unsigned *end = s + size / sizeof(unsigned);
+    while(s != end)
+        *d++ = *s++;
+}
+
+static char buffer[4096];
+
 int main(int argc, char **argv) {
     if(argc < 2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
         exit(1);
     }
 
-    unsigned long pagesize = sysconf(_SC_PAGESIZE);
     unsigned start1 = get_cycles();
     int fd = open(argv[1], O_RDONLY);
     off_t total = lseek(fd, 0, SEEK_END);
     void *addr = mmap(NULL, total, PROT_READ, MAP_PRIVATE, fd, 0);
     unsigned start2 = get_cycles();
 
-    char *p = (char*)addr;
-    char *end = p + total;
-    while(p < end) {
-        __attribute__((unused)) volatile char fetch = *p;
-        p += pagesize;
+    off_t pos = 0;
+    while(pos < total) {
+        size_t amount = (size_t)(total - pos) < sizeof(buffer) ? (size_t)(total - pos) : sizeof(buffer);
+        copy(buffer, addr + pos, amount);
+        pos += amount;
     }
 
     unsigned end1 = get_cycles();
 
     unsigned start3 = get_cycles();
-    p = (char*)addr;
-    end = p + total;
-    while(p < end) {
-        __attribute__((unused)) volatile char fetch = *p;
-        p += pagesize;
+    pos = 0;
+    while(pos < total) {
+        size_t amount = (size_t)(total - pos) < sizeof(buffer) ? (size_t)(total - pos) : sizeof(buffer);
+        copy(buffer, addr + pos, amount);
+        pos += amount;
     }
 
     unsigned end3 = get_cycles();
@@ -42,7 +51,7 @@ int main(int argc, char **argv) {
     close(fd);
     unsigned end2 = get_cycles();
 
-    printf("Total bytes: %zu\n", (size_t)total);
+    printf("Total bytes: %zu\n", (size_t)pos);
     printf("Total time: %u\n", end2 - start1);
     printf("Open time: %u\n", start2 - start1);
     printf("Read time: %u\n", end1 - start2);
