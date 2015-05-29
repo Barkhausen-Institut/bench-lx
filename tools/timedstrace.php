3611d7a01e53fab951456f85d908affe8be0e6fd
Nils Asmussen <nils@script-solution.de>
2015-05-29 11:49:58 +0200

    Improved timedstrace.php.

--- tools/timedstrace.php
+++ tools/timedstrace.php
@@ -24,20 +24,27 @@ if($argc != 3)
 $strace = file($argv[1]);
 $times = file($argv[2]);
 
+$last = 0;
 $timestamp = 0;
 foreach($times as $i => $time) {
     preg_match('/^\s*\[\s*\d+\]\s+(\d+)\s+(\d+)\s+(\d+)/', $time, $ti);
-    preg_match('/^(.+?)\(/', $strace[$i], $st);
+    preg_match('/^(.+?)\(([^,]*)/', $strace[$i], $st);
+
+    $last = $ti[3];
 
     // ignore exec because we don't want to compare the loading
     if($st[1] == 'execve')
+        continue;
+    // ignore writes to stdout/stderr
+    if($st[1] == 'write' && ($st[2] == '1' || $st[2] == '2'))
         continue;
 
     if(array_search($st[1], $names) !== FALSE) {
         if(@$names[$ti[1]] != $st[1])
             @exit("Line $i: expected syscall " . $names[$ti[1]] . ", got " . $st[1] . "\n");
 
-        if($timestamp > 0)
+        // ignore waits of less than 1000 cycles.
+        if($timestamp > 0 && ($ti[2] - $timestamp) > 1000)
             echo "_waituntil(" . ($ti[2] - $timestamp) . ") = 0\n";
         $timestamp = $ti[3];
     }
@@ -45,4 +52,7 @@ foreach($times as $i => $time) {
         @$timestamp = $ti[2];
     echo $strace[$i];
 }
+
+if($timestamp > 0 && ($last - $timestamp) > 1000)
+    echo "_waituntil(" . ($last - $timestamp) . ") = 0\n";
 ?>
