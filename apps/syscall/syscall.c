bd144386689ba1e34dc2646b821ecb72a2b4b6e7
Nils Asmussen <nils@script-solution.de>
2015-07-01 16:26:59 +0200

    syscall: simply throw away the times that are way off.

--- apps/syscall/syscall.c
+++ apps/syscall/syscall.c
@@ -5,6 +5,7 @@
 #define COUNT	SYSCALL_REPEAT
 
 static cycle_t times[COUNT];
+static cycle_t nice_times[COUNT];
 
 int main() {
 	cycle_t start = get_cycles();
@@ -15,7 +16,14 @@ int main() {
 		start = cyc;
 	}
 
-    cycle_t average = avg(times, COUNT);
-    printf("[syscall] Time per syscall (avg): %lu (%lu)\n", average, stddev(times, COUNT, average));
+	// simply throw away the times that are way off (due to timer irqs etc.)
+	int nice = 0;
+	for(i = 0; i < COUNT; ++i) {
+		if(times[i] < 1000)
+			nice_times[nice++] = times[i];
+	}
+
+    cycle_t average = avg(nice_times, nice);
+    printf("[syscall] Time per syscall (avg): %lu (%lu)\n", average, stddev(nice_times, nice, average));
 	return 0;
 }
