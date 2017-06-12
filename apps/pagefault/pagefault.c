9225528e44d375cd652b7a347378c2bb482bde6b
Nils Asmussen <nils@script-solution.de>
2017-06-12 14:02:21 +0200

    Modified pagefault benchmark.

--- apps/pagefault/pagefault.c
+++ apps/pagefault/pagefault.c
@@ -13,24 +13,19 @@
 #include <smemcpy.h>
 
 #define PAGE_SIZE   4096
+#define COUNT       8
 #define PAGES       64
-#define STEPWIDTH   1
-#define COUNT       PAGES / STEPWIDTH
 
 static cycle_t anontimes[COUNT];
 static cycle_t filetimes[COUNT];
 
-static void do_access(char *data, cycle_t *times) {
-    for(size_t i = 0; i < COUNT; ++i) {
-        cycle_t start = get_cycles();
-        data[i * STEPWIDTH * PAGE_SIZE] = 1;
-        cycle_t end = get_cycles();
-        times[i] = end - start;
-    }
+static void do_access(volatile char *data) {
+    for(size_t i = 0; i < PAGES; ++i)
+        data[i * PAGE_SIZE];
 }
 
 int main() {
-    {
+    for(size_t i = 0; i < COUNT; ++i) {
         char *data = (char*)mmap(NULL, PAGES * PAGE_SIZE,
             PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
         if(data == MAP_FAILED) {
@@ -38,13 +33,16 @@ int main() {
             return 1;
         }
 
-        do_access(data, anontimes);
+        cycle_t start = get_cycles();
+        do_access(data);
+        cycle_t end = get_cycles();
+        anontimes[i] = end - start;
 
         munmap(data, PAGES * PAGE_SIZE);
     }
 
-    {
-        int fd = open("/large.bin", O_RDONLY);
+    for(size_t i = 0; i < COUNT; ++i) {
+        int fd = open("/bench/large.bin", O_RDONLY);
         if(fd == -1) {
             perror("open");
             return 1;
@@ -56,7 +54,10 @@ int main() {
             return 1;
         }
 
-        do_access(data, filetimes);
+        cycle_t start = get_cycles();
+        do_access(data);
+        cycle_t end = get_cycles();
+        filetimes[i] = end - start;
 
         munmap(data, PAGES * PAGE_SIZE);
         close(fd);
