3d67ab53cc0e31ca30241a828a3e883bf5448cb9
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-08-02 14:51:33 +0200

    Support multiple runs of fs benchmarks.

--- benchfs/fsbench.sh
+++ benchfs/fsbench.sh
@@ -7,7 +7,13 @@ cat /tmp/res.txt
 
 echo "===="
 
-rm /tmp/test.db 2>/dev/null
-/bench/bin/stracetime $1 2>/dev/null
+for i in 1 2 3 4 5 6 7 8; do
+    rm /tmp/test.db 2>/dev/null
+    /bench/bin/stracetime $1 2>/dev/null
+
+    if [ $i -ne 8 ]; then
+        echo "############"
+    fi
+done
 
 echo "<===Benchmarks_done==="
