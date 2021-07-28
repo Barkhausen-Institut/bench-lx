ec441a4a6ed61d384b762e91cd10e69a3428d38b
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-07-28 15:03:08 +0200

    build: fixed issues when building for x86_64.

--- include/sysctrace.h
+++ include/sysctrace.h
@@ -3,7 +3,11 @@
 #include <sys/syscall.h>
 #include <common.h>
 
+#if defined __THROW
+extern long syscall(long, ...) __THROW;
+#else
 extern long syscall(long, ...);
+#endif
 
 static inline void syscreset(int pid) {
     syscall(SYS_SYSCRESET, pid, 0);
