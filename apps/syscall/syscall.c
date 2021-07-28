c0afffa83499e5cf0169be4febe597fc2e81818f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-07-28 15:31:40 +0200

    Simplified syscall benchmark.

--- apps/syscall/syscall.c
+++ apps/syscall/syscall.c
@@ -1,34 +1,24 @@
 #include <cycles.h>
 #include <common.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define WARMUP 	5
 #define COUNT	100
 
 static cycle_t times[COUNT];
-static cycle_t nice_times[COUNT];
 
 int main() {
-	cycle_t start;
-	int i;
+	for(int i = 0; i < WARMUP; ++i)
+		geteuid();
 
-	for(i = 0; i < WARMUP; ++i)
-		start = get_cycles();
-
-	for(i = 0; i < COUNT; ++i) {
-		cycle_t cyc = get_cycles();
-		times[i] = cyc - start;
-		start = cyc;
+	for(int i = 0; i < COUNT; ++i) {
+		cycle_t start = get_cycles();
+		geteuid();
+		times[i] = get_cycles() - start;
 	}
 
-	// simply throw away the times that are way off (due to timer irqs etc.)
-	int nice = 0;
-	for(i = 0; i < COUNT; ++i) {
-		if(times[i] < 1000)
-			nice_times[nice++] = times[i];
-	}
-
-    cycle_t average = avg(nice_times, nice);
-    printf("%lu %lu\n", average, stddev(nice_times, nice, average));
+    cycle_t average = avg(times, COUNT);
+    printf("%lu %lu\n", average, stddev(times, COUNT, average));
 	return 0;
 }
