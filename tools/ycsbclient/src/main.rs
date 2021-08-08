d0aaf13c8065ec11c1e8314ed7a3f9fb60098e4f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-08-08 13:51:11 +0200

    ycsbclient: multiple repeats and optional ACKs.

--- tools/ycsbclient/src/main.rs
+++ tools/ycsbclient/src/main.rs
@@ -1,7 +1,8 @@
 #![feature(with_options)]
 
+use std::net::TcpListener;
 use std::io::{BufReader, Read, Write};
-use std::net::TcpStream;
+
 
 mod importer;
 
@@ -16,53 +17,67 @@ fn print_help() {
 ---- ycsb workload sender for M³'s key value store benchmark ----
 
 USAGE:
-standalone_client <file> <ip> <port>
+standalone_client <file> <ip> <port> <acks> <repeats>
 
 Example:
-ycsb_standalone_client workload.wl 127.0.0.2 1337
+ycsb_standalone_client workload.wl 127.0.0.2 1337 true 4
 
 <file> : Specifies the workloadfile thats being used. Must be generated via the ycsb_parser tool
 <IP>   : Destination IP,
 <PORT> : Destination port
+<acks> : whether to expect ACKs from the other side
 "
     );
 }
 
 fn main() {
     // Parse arguments
-    // workaround for riscv64-*-musl: apparently, the arguments don't work, so hardcode them
-    // let args = vec!["whatever", "/bench/workload.wl", "127.0.0.1", "1337"];
-
     let args = std::env::args().map(|a| a.to_string()).collect::<Vec<_>>();
-    if args.len() != 4 {
+    if args.len() != 6 {
         print_help();
         return;
     }
 
-    // Get file
-    let workload_file = std::fs::File::open(&args[1]).expect("Could not open file");
-    let mut workload = BufReader::new(workload_file);
-    println!("Trying to connect to {}:{}", args[2], args[3]);
-    let mut socket = TcpStream::connect(format!("{}:{}", args[2], args[3]))
-        .expect("Failed to connect, is the server running?");
+    let acks = args[4].parse::<bool>().expect("parsing ACK failed");
+    let repeats = args[5].parse::<u32>().expect("parsing repeats failed");
 
-    // Now send requests until the workload file is empty
-    let num_requests = importer::WorkloadHeader::load_from_file(&mut workload).number_of_operations;
+    let ep = format!("{}:{}", args[2], args[3]);
+    let listener = TcpListener::bind(ep).expect("bind failed");
 
-    for idx in 0..num_requests {
-        let new_req = importer::Package::load_as_bytes(&mut workload);
-        let len = (new_req.len() as u32).to_be_bytes();
-        socket.write(&len).unwrap();
-        let written = socket.write(&new_req).expect("Failed to send request");
-        assert!(written == new_req.len(), "Did not send whole request");
+    loop {
+        println!("waiting for TCP connection on {}:{}", args[2], args[3]);
 
-        if (idx + 1) % 16 == 0 {
-            socket.read(&mut [0u8; 1]).expect("receive failed");
+        // accept connections and process them serially
+        let (mut socket, addr) = listener.accept().expect("accept failed");
+        println!("accepted connection from {}", addr);
+
+        for r in 0..repeats {
+            // Get file
+            let workload_file = std::fs::File::open(&args[1]).expect("Could not open file");
+            let mut workload = BufReader::new(workload_file);
+
+            // Now send requests until the workload file is empty
+            let num_requests = importer::WorkloadHeader::load_from_file(&mut workload).number_of_operations;
+
+            for idx in 0..num_requests {
+                let new_req = importer::Package::load_as_bytes(&mut workload);
+                let len = (new_req.len() as u32).to_be_bytes();
+                socket.write(&len).unwrap();
+                let written = socket.write(&new_req).expect("Failed to send request");
+                assert!(written == new_req.len(), "Did not send whole request");
+
+                if acks && (idx + 1) % 16 == 0 {
+                    socket.read(&mut [0u8; 1]).expect("receive failed");
+                }
+            }
+
+            if r + 1 < repeats {
+                socket.write(&(6 as u32).to_be_bytes()).unwrap();
+                socket.write(b"ENDRUN").unwrap();
+            }
         }
+
+        socket.write(&(6 as u32).to_be_bytes()).unwrap();
+        socket.write(b"ENDNOW").unwrap();
     }
-
-    socket.write(&(6 as u32).to_be_bytes()).unwrap();
-    socket.write(b"ENDNOW").unwrap();
-
-    println!("End");
 }
