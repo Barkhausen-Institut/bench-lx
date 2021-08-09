3ed44d45a297429b0f739c0b254fa51e82487753
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-08-09 06:51:51 +0200

    exec: removed unnecessary stuff.

--- apps/exec/exec.c
+++ apps/exec/exec.c
@@ -15,16 +15,14 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Usage: %s <runs> <program> [<args>...]\n", argv[0]);
         exit(1);
     }
+
     size_t runs = strtoul(argv[1], NULL, 10);
 
     size_t i;
     for(i = 0; i < runs; ++i) {
-        cycle_t start = prof_start(0x1234);
-
         int pid;
         switch((pid = fork())) {
             case 0: {
-                syscreset(getpid());
                 execv(argv[2], argv + 2);
                 perror("exec");
                 break;
@@ -34,9 +32,6 @@ int main(int argc, char **argv) {
                 break;
             default: {
                 waitpid(pid, NULL, 0);
-                cycle_t end = prof_stop(0x1234);
-                printf("Execution took %lu cycles\n", end - start);
-                sysctrace();
                 break;
             }
         }
