88a39015c2f8aa3a55717e2cd19cd51a2e266afd
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-02-11 12:16:17 +0100

    Fixed pingpong benchmark.

--- rootfs/bench/pingpong.sh
+++ rootfs/bench/pingpong.sh
@@ -2,13 +2,13 @@
 
 echo ">===Starting_benchmarks==="
 
-msgsize=$2
-if [ "$1" = "2" ]; then
+msgsize=$1
+if [ "$2" = "2" ]; then
     /bench/bin/pingpong 0 1 1 "$msgsize"
-elif [ "$1" = "4" ]; then
+elif [ "$2" = "4" ]; then
     /bench/bin/pingpong 0 1 10 "$msgsize" &
     /bench/bin/pingpong 2 3 1 "$msgsize"
-elif [ "$1" = "6" ]; then
+elif [ "$2" = "6" ]; then
     /bench/bin/pingpong 0 1 10 "$msgsize" &
     /bench/bin/pingpong 2 3 10 "$msgsize" &
     /bench/bin/pingpong 4 5 1 "$msgsize"
